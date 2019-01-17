ed37a6c68caba7982db75b3185fc12001b38f588
ohoon <46547443+ohoon@users.noreply.github.com>
2019-01-17 13:41:44 +0900

    Update PostManager.py

--- PostManager.py
+++ PostManager.py
@@ -6,8 +6,7 @@ import tkinter.ttk
 import re
 import webbrowser
 
-
-def search():
+def start():
     try:
         fst = int(firstE.get())
         lst = int(lastE.get())
@@ -22,15 +21,71 @@ def search():
 
 
 def result(f, l):
+    global count
+    global rcount
+
+    def add():
+        global count
+        global rcount
+
+        try:
+            s = int(r_firstE.get())
+            e = int(r_lastE.get())
+            if s > e:
+                raise ValueError
+            if len(str(s)) != 13 or len(str(e)) != 13:
+                raise ValueError
+        except:
+            messagebox.showinfo("Error!", "올바른 값을 입력해주세요.")
+        else:
+            for pnum in range(s, e + 1):
+                with url.urlopen("https://service.epost.go.kr/trace.RetrieveDomRigiTraceList.comm?sid1=" + str(
+                        pnum) + "&displayHeader=N") as doc:
+                    html = doc.read().decode()
+                    soup = BeautifulSoup(html, features="lxml")
+                    data = soup.find("table", "table_col").find("tbody").find("tr").find_all("td")
+                    name = str(data[1])[4:-5].split("<br/>")[0]
+                    date = str(data[1])[4:-5].split("<br/>")[1]
+                    status = re.sub("\(.*\)", '', data[3].get_text())
+
+                    if status == "배달완료" or status == "교부":
+                        s_data = re.findall("\(수령인:.*\)", str(soup))
+                        s_status = re.sub("\(수령인:|\)", '', s_data[0])
+                        succT.insert("", "end", text="", values=[pnum, name, s_status, date], iid=pnum)
+                        count += 1
+                    elif status == "배달준비":
+                        retrnT.insert("", "end", text="", values=[pnum, name, s_status, date], iid=pnum)
+                        rcount += 1
+                    elif status == "" or status == "도착":
+                        retrnT.insert("", "end", text="", values=[pnum, name, s_status, date], iid=pnum)
+                        rcount += 1
+                    elif status == "미배달":
+                        r_data = re.findall("<!-- 미배달사유 -->.*\s*.*\s*.*<!-- //미배달사유 -->", str(soup))
+                        r_status = re.sub("<.+?>|\s", '', r_data[len(r_data) - 1])
+                        retrnT.insert("", "end", text="", values=[pnum, name, r_status, date], iid=pnum)
+                        rcount += 1
+
+            resultL.configure(text="배달완료 " + str(count) + "건\t미배달 " + str(rcount) + "건\t총 " + str(count + rcount) + "건")
 
     def pop_up_s(event):
         webbrowser.open("https://service.epost.go.kr/trace.RetrieveDomRigiTraceList.comm?sid1=" + str(succT.focus()) + "&displayHeader=N")
     def pop_up_r(event):
         webbrowser.open("https://service.epost.go.kr/trace.RetrieveDomRigiTraceList.comm?sid1=" + str(retrnT.focus()) + "&displayHeader=N")
 
+    def sort_column(tv, col, reverse):
+        l = [(tv.set(k, col), k) for k in tv.get_children('')]
+        l.sort(reverse=reverse)
+
+        # rearrange items in sorted positions
+        for index, (val, k) in enumerate(l):
+            tv.move(k, '', index)
+
+            # reverse sort next time
+        tv.heading(col, command=lambda: sort_column(tv, col, not reverse))
+
     resultC = tk.Toplevel(mainC)
     resultC.title("조회 결과")
-    resultC.geometry("736x450+495+300")
+    resultC.geometry("736x460+495+300")
     resultC.resizable(0, 0)
 
     succF = tk.Frame(resultC, bd=5)
@@ -38,17 +93,18 @@ def result(f, l):
     succL = tk.Label(succF, text="배달완료", font="함초롬돋움 12")
     succL.grid(column=0, columnspan=2, row=0)
 
-    succT = tkinter.ttk.Treeview(succF, columns=["", "", ""], height=18)
+    succT = tkinter.ttk.Treeview(succF, columns=["", "", "", ""], height=18)
     succT.grid(column=0, columnspan=2, row=1)
     succT.yview()
-    succT.column("#0", width=120, anchor="center")
-    succT.heading("#0", text="등기번호", anchor="center")
-    succT.column("#1", width=45, anchor="center")
-    succT.heading("#1", text="성명", anchor="center")
-    succT.column("#2", width=110, anchor="center")
-    succT.heading("#2", text="수령인", anchor="center")
-    succT.column("#3", width=80, anchor="center")
-    succT.heading("#3", text="수령일자", anchor="center")
+    succT["show"] = "headings"
+    succT.column("#1", width=120, anchor="center")
+    succT.heading("#1", text="등기번호", anchor="center", command=lambda: sort_column(succT, "#1", False))
+    succT.column("#2", width=45, anchor="center")
+    succT.heading("#2", text="성명", anchor="center", command=lambda: sort_column(succT, "#2", False))
+    succT.column("#3", width=110, anchor="center")
+    succT.heading("#3", text="수령인", anchor="center", command=lambda: sort_column(succT, "#3", False))
+    succT.column("#4", width=80, anchor="center")
+    succT.heading("#4", text="수령일자", anchor="center", command=lambda: sort_column(succT, "#4", False))
     succT.bind('<<TreeviewOpen>>', pop_up_s)
 
     retrnF = tk.Frame(resultC, bd=5)
@@ -56,27 +112,23 @@ def result(f, l):
     retrnL = tk.Label(retrnF, text="미배달", font="함초롬돋움 12")
     retrnL.grid(column=0, columnspan=2, row=0)
 
-    retrnT = tkinter.ttk.Treeview(retrnF, columns=["", "", ""], height=18)
+    retrnT = tkinter.ttk.Treeview(retrnF, columns=["", "", "", ""], height=18)
     retrnT.grid(column=0, columnspan=2, row=1)
     retrnT.yview()
-    retrnT.column("#0", width=120, anchor="center")
-    retrnT.heading("#0", text="등기번호", anchor="center")
-    retrnT.column("#1", width=45, anchor="center")
-    retrnT.heading("#1", text="성명", anchor="center")
-    retrnT.column("#2", width=110, anchor="center")
-    retrnT.heading("#2", text="처리현황", anchor="center")
-    retrnT.column("#3", width=80, anchor="center")
-    retrnT.heading("#3", text="처리일자", anchor="center")
+    retrnT["show"] = "headings"
+    retrnT.column("#1", width=120, anchor="center")
+    retrnT.heading("#1", text="등기번호", anchor="center", command=lambda: sort_column(retrnT, "#1", False))
+    retrnT.column("#2", width=45, anchor="center")
+    retrnT.heading("#2", text="성명", anchor="center", command=lambda: sort_column(retrnT, "#2", False))
+    retrnT.column("#3", width=110, anchor="center")
+    retrnT.heading("#3", text="처리현황", anchor="center", command=lambda: sort_column(retrnT, "#3", False))
+    retrnT.column("#4", width=80, anchor="center")
+    retrnT.heading("#4", text="처리일자", anchor="center", command=lambda: sort_column(retrnT, "#4", False))
     retrnT.bind('<<TreeviewOpen>>', pop_up_r)
-
-    resultF = tk.Frame(resultC)
-    resultF.grid(column=0, columnspan=4, row=4)
-    resultL = tk.Label(resultF, text="", font="함초롬돋움 10", anchor="center")
-    resultL.grid(column=0, row=0)
 
     count = 0
     rcount = 0
-    for pnum in range(f, l+1):
+    for pnum in range(f, l + 1):
         with url.urlopen("https://service.epost.go.kr/trace.RetrieveDomRigiTraceList.comm?sid1=" + str(
                 pnum) + "&displayHeader=N") as doc:
             html = doc.read().decode()
@@ -89,20 +141,32 @@ def result(f, l):
             if status == "배달완료" or status == "교부":
                 s_data = re.findall("\(수령인:.*\)", str(soup))
                 s_status = re.sub("\(수령인:|\)", '', s_data[0])
-                succT.insert("", "end", text=pnum, values=[name, s_status, date], iid=pnum)
+                succT.insert("", "end", text="", values=[pnum, name, s_status, date], iid=pnum)
                 count += 1
             elif status == "배달준비":
-                retrnT.insert("", "end", text=pnum, values=[name, status, date], iid=pnum)
+                retrnT.insert("", "end", text="", values=[pnum, name, s_status, date], iid=pnum)
                 rcount += 1
             elif status == "" or status == "도착":
-                retrnT.insert("", "end", text=pnum, values=[name, status, date], iid=pnum)
+                retrnT.insert("", "end", text="", values=[pnum, name, s_status, date], iid=pnum)
                 rcount += 1
             elif status == "미배달":
                 r_data = re.findall("<!-- 미배달사유 -->.*\s*.*\s*.*<!-- //미배달사유 -->", str(soup))
-                r_status = re.sub("<.+?>|\s", '', r_data[len(r_data)-1])
-                retrnT.insert("", "end", text=pnum, values=[name, r_status, date], iid=pnum)
+                r_status = re.sub("<.+?>|\s", '', r_data[len(r_data) - 1])
+                retrnT.insert("", "end", text="", values=[pnum, name, r_status, date], iid=pnum)
                 rcount += 1
-    resultL.configure(text="배달완료 " + str(count) + "건\t미배달 " + str(rcount) + "건\t총 " + str(count+rcount) + "건")
+
+    resultF = tk.Frame(resultC)
+    resultF.grid(column=0, columnspan=4, row=4)
+    resultL = tk.Label(resultF, text="배달완료 " + str(count) + "건\t미배달 " + str(rcount) + "건\t총 " + str(count + rcount) + "건", font="함초롬돋움 10", anchor="center", padx=25)
+    resultL.grid(column=0, row=0)
+    r_firstE = tk.Entry(resultF)
+    r_firstE.grid(column=1, row=0, padx=5)
+    r_Label = tk.Label(resultF, text="~")
+    r_Label.grid(column=2, row=0)
+    r_lastE = tk.Entry(resultF)
+    r_lastE.grid(column=3, row=0, padx=5)
+    resultB = tk.Button(resultF, text="이어서 조회", width=10, font="함초롬돋움 10", command=add)
+    resultB.grid(column=4, row=0)
 
 mainC = tk.Tk()
 mainC.title("등기번호 조회기")
@@ -122,6 +186,6 @@ lastL.grid(column=0, row=2)
 lastE = tk.Entry(mainC)
 lastE.grid(column=1, row=2)
 
-mainB = tk.Button(mainC, text="조회하기", width=15, font="함초롬돋움 13", command=search)
+mainB = tk.Button(mainC, text="조회하기", width=15, font="함초롬돋움 13", command=start)
 mainB.grid(column=0, columnspan=2, row=3)
 mainC.mainloop()
